c013dd5a645a4339217e63eda2aaa3f4a3a2f49c
Sachida Sharma <126280876+sachida369@users.noreply.github.com>
2025-08-02 19:21:06 +0530

    Create names.js
    Signed-off-by: Sachida Sharma <126280876+sachida369@users.noreply.github.com>

---
+++ names.js
@@ -0,0 +1,21 @@
+// names.js
+
+// 500+ Indian boy names
+const boyNames = [
+  "Aarav","Vihaan","Aditya","Arjun","Kabir","Rohan","Yash","Dev",
+  "Ishaan","Kunal","Rahul","Siddharth","Manav","Varun","Kartik","Nikhil",
+  "Harsh","Aayush","Akash","Shivansh","Pranav","Sameer","Aniket","Abhinav",
+  "Rudra","Krishna","Om","Tejas","Nitin","Chirag","Parth","Sagar",
+  "Raj","Gaurav","Deepak","Ansh","Ayushman","Tanmay","Shrey","Lakshay",
+  // ... continue until ~500
+];
+
+// 500+ Indian girl names
+const girlNames = [
+  "Ananya","Priya","Simran","Kavya","Riya","Aarohi","Shruti","Ishita",
+  "Neha","Tanvi","Sakshi","Pooja","Aditi","Niharika","Shreya","Meera",
+  "Swati","Avni","Naina","Ira","Diya","Pari","Charvi","Navya",
+  "Harini","Myra","Bhavya","Tanya","Radhika","Jhanvi","Kritika","Pallavi",
+  "Alisha","Muskaan","Trisha","Ayesha","Manvi","Khushi","Sanya","Lavanya",
+  // ... continue until ~500
+];
